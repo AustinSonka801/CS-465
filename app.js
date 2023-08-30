b135ad4b903fa1afb3bf4136125729acd2ab9649
Austin Sonka <austinsonka.atlas@gmail.com>
2023-08-30 05:13:17 -0500

    Convert travel template from static to dynamic JSON

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// include reference to handlebars code
+const hbs = require('hbs');
 
-var indexRouter = require('./app_server/routes/index');
+// set up routers
+var indexRouter = require('./app_server/routes/index_router');
 var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel_router');
+var aboutRouter = require('./app_server/routes/about_router');
+var contactRouter = require('./app_server/routes/contact_router');
+var mealsRouter = require('./app_server/routes/meals_router');
+var newsRouter = require('./app_server/routes/news_router');
+var roomsRouter = require('./app_server/routes/rooms_router');
 
 var app = express();
 
@@ -13,14 +22,24 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
+// register handlebars partials (https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
