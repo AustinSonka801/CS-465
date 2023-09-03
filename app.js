9dcd634b4727e0db40866014f463035d960a6a4e
Austin Sonka <austinsonka.atlas@gmail.com>
2023-09-03 07:31:54 -0500

    Create new application API

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // include reference to handlebars code
 const hbs = require('hbs');
+require('./app_api/database/db');
 
 // set up routers
 var indexRouter = require('./app_server/routes/index_router');
@@ -15,6 +16,7 @@ var contactRouter = require('./app_server/routes/contact_router');
 var mealsRouter = require('./app_server/routes/meals_router');
 var newsRouter = require('./app_server/routes/news_router');
 var roomsRouter = require('./app_server/routes/rooms_router');
+const apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -40,6 +42,7 @@ app.use('/contact', contactRouter);
 app.use('/meals', mealsRouter);
 app.use('/news', newsRouter);
 app.use('/rooms', roomsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,32 +1,38 @@
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
-const dbURL = 'mongodb://${host}/travlr';
+const dbURL = `mongodb://${host}/travlr`;
 const readLine = require('readline');
 
-const {seed} = require('./seed');
-
-mongoose.set('useUnifiedTopology', true);
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
+}
 
 const connect = () => {
-  setTimeout(() => mongoose.connect(dbURL, {
-  
-      useNewUrlParser: true,
-      useCreateIndex: true 
-    }), 1000);
-    }
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }), 1000);
+}
 
-
-    
 mongoose.connection.on('connected', () => {
   console.log('connected');
 });
+
 mongoose.connection.on('error', err => {
   console.log('error: ' + err);
   return connect();
 });
+
 mongoose.connection.on('disconnected', () => {
   console.log('disconnected');
 });
+
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
 
 const gracefulShutdown = (msg, callback) => {
   mongoose.connection.close( () => {
